1f91e161fddaa7c648319033f21b294bb5c4a0a7
Yogeshmaari <151515524+Yogeshmaari@users.noreply.github.com>
2023-11-30 10:28:53 +0530

    push

--- Crud.py
+++ Crud.py
@@ -1,21 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,Query
 from pydantic import BaseModel
+from typing import Optional
 
 
 app = FastAPI()
 
+#storing the user details
 user = []
-
-@app.get("/")
-def read():
-    return user
 
 class Model(BaseModel):
     name: str
     age:int
     address: str
-    
-@app.post("/post")
+#read the user detailss
+@app.get("/User")
+def read():
+    return user
+
+#post the user details
+@app.post("/User")
 def create(req:Model):
     user.append(req)
     return req
+
+#We give the id and its give the id details 
+@app.get("/User/{id}")
+#29th line id we want to use interger and use path also 
+def get_user(id:int=Path(...,description="The Id of the user you want to retrived.",gt=2),AddNewData:str = Query(None,max_length=5) ): 
+    return {"Userid": user[id],"Query":AddNewData}
